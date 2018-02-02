fc12ffd859c1d44dfc7aab6e3cb1ea89f6daf78f
Connor Gramazio <connor@cs.brown.edu>
2018-02-01 21:11:45 -0500

    Initial implementation.

---
+++ js/drawPlots.js
@@ -0,0 +1,179 @@
+function drawPlots(allColors) {
+  allColors = allColors.map(d => d3.lab(d));
+
+  // subsample colors
+  var delta = 10,
+      colors = [],
+      itr = allColors.length < 8 ? 1 : Math.floor(allColors.length/8);
+
+  if(allColors.length % 8 !== 0 && itr > 1) itr--; // we always want to include the endpoints
+  for (var i = 0; i < allColors.length; i=i+itr) {
+    if(allColors[i] === undefined) continue;
+    colors.push(allColors[i]);
+  }
+  if(allColors % 8 !== 0 && allColors.length > 9) colors.push(allColors[allColors.length-1]);
+
+  var container = d3.select("body").append("div").style("margin-bottom", "2em"),
+      svgClasses = ["plot_ab", "plot_la", "plot_lb"];
+      plots = container.selectAll("svg")
+          .data(svgClasses).enter().append("svg")
+          .classed("plot", true);
+
+  plots.each(function(d) { d3.select(this).classed(d, true); });
+
+  var width = 200,
+      height = width,
+      fullWidth = width,
+      margin = { top: 25, right: 25, bottom: 25, left: 25 };
+
+  plots.style("width", width + "px").style("height", height + "px");
+
+  width = width - margin.left - margin.right;
+  height = height - margin.top - margin.bottom;
+
+  plots.append("g")
+      .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+  // x and y refer to when a and b are either on the x or y axis s
+  var scale_l = d3.scaleLinear().domain([0, 100]).range([height, 0]),
+      scale_ab_x = d3.scaleLinear().domain([-100, 100]).range([0, height]),
+      scale_ab_y = d3.scaleLinear().domain([-100, 100]).range([height, 0]);
+
+  var plot_ab = container.select(".plot_ab").select("g"),
+      plot_la = container.select(".plot_la").select("g"),
+      plot_lb = container.select(".plot_lb").select("g");
+
+  var xaxis_ab = d3.axisBottom(scale_ab_x),
+      yaxis_ab = d3.axisLeft(scale_ab_y),
+      yaxis_l = d3.axisLeft(scale_l);
+
+  var lineFn_ab = d3.line().x(d => scale_ab_x(d.a)).y(d => scale_ab_y(d.b)),
+      lineFn_la = d3.line().x(d => scale_ab_x(d.a)).y(d => scale_l(d.l)),
+      lineFn_lb = d3.line().x(d => scale_ab_x(d.b)).y(d => scale_l(d.l));
+
+  plot_ab.append("g").classed("axisGroup", true).attr("transform", "translate(0,"+(height/2)+")").call(xaxis_ab);
+  plot_ab.append("g").classed("axisGroup", true).attr("transform", "translate("+(width/2)+",0)").call(yaxis_ab);
+  plot_ab.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 0).text("x: a*");
+  plot_ab.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 12).text("y: b*");
+  plot_ab.append("path").attr("d", lineFn_ab(allColors))
+      .attr("stroke", "rgba(0,0,0,0.75)").attr("stroke-width", 1).attr("fill", "none");
+  plot_ab.selectAll("circle")
+      .data(colors)
+      .enter()
+      .append("circle")
+          .attr("r", 5)
+          .attr("cx", d => scale_ab_x(d.a))
+          .attr("cy", d => scale_ab_y(d.b))
+          .attr("fill", d => d);
+
+  plot_la.append("g").classed("axisGroup", true).attr("transform", "translate(0,"+height+")").call(xaxis_ab);
+  plot_la.append("g").classed("axisGroup", true).attr("transform", "translate(0,0)").call(yaxis_l);
+  plot_la.append("line")
+      .attr("x1", width/2).attr("x2", width/2)
+      .attr("y1", 0).attr("y2", height)
+      .style("stroke-width", 1)
+      .style("stroke", "#ddd");
+  plot_la.append("rect")
+      .attr("y", scale_l(0) - 7).attr("x", scale_ab_x(0) - 7)
+      .attr("width", 14).attr("height", 14).attr("fill", "black")
+      .attr("stroke", "black").attr("stroke-width", 1);
+  plot_la.append("rect")
+      .attr("y", scale_l(100) - 7).attr("x", scale_ab_x(0) - 7)
+      .attr("width", 14).attr("height", 14).attr("fill", "white")
+      .attr("stroke", "black").attr("stroke-width", 1);
+  plot_la.append("path").attr("d", lineFn_la(allColors))
+      .attr("stroke", "rgba(0,0,0,0.75)").attr("stroke-width", 1).attr("fill", "none");
+  plot_la.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 0).text("x: a*");
+  plot_la.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 12).text("y: l*");
+  plot_la.selectAll("circle")
+      .data(colors)
+      .enter()
+      .append("circle")
+          .attr("r", 5)
+          .attr("cx", d => scale_ab_x(d.a))
+          .attr("cy", d => scale_l(d.l))
+          .attr("fill", d => d);
+
+  plot_lb.append("g").classed("axisGroup", true).attr("transform", "translate(0,"+height+")").call(xaxis_ab);
+  plot_lb.append("g").classed("axisGroup", true).attr("transform", "translate(0,0)").call(yaxis_l);
+  plot_lb.append("line")
+      .attr("x1", width/2).attr("x2", width/2)
+      .attr("y1", 0).attr("y2", height)
+      .style("stroke-width", 1)
+      .style("stroke", "#ddd");
+  plot_lb.append("path").attr("d", lineFn_lb(allColors))
+      .attr("stroke", "rgba(0,0,0,0.75)").attr("stroke-width", 1).attr("fill", "none");
+  plot_lb.append("rect")
+      .attr("y", scale_l(0) - 7).attr("x", scale_ab_x(0) - 7)
+      .attr("width", 14).attr("height", 14).attr("fill", "black")
+      .attr("stroke", "black").attr("stroke-width", 1);
+  plot_lb.append("rect")
+      .attr("y", scale_l(100) - 7).attr("x", scale_ab_x(0) - 7)
+      .attr("width", 14).attr("height", 14).attr("fill", "white")
+      .attr("stroke", "black").attr("stroke-width", 1);
+  plot_lb.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 0).text("x: b*");
+  plot_lb.append("text")
+      .classed("label", true)
+      .attr("x", 5).attr("y", 12).text("y: l*");
+  plot_lb.selectAll("circle")
+      .data(colors)
+      .enter()
+      .append("circle")
+          .attr("r", 5)
+          .attr("cx", d => scale_ab_x(d.b))
+          .attr("cy", d => scale_l(d.l))
+          .attr("fill", d => d);
+
+  plots.selectAll(".label")
+      .style("font-size", "10").style("font-family", "sans-serif");
+
+  plots.selectAll("circle").style("stroke", "black").style("stroke-width", 1);
+
+  plots.selectAll(".axisGroup").selectAll(".tick")
+      .each(function(d,i) {
+        if(i % 2) d3.select(this).select("text").remove();
+      });
+
+  plots.attr("version", 1.1).attr("baseProfile", "full")
+      .attr("width", fullWidth).attr("height", fullWidth)
+      .attr("xmlns", "http://www.w3.org/2000/svg");
+
+
+  var tbl = container.append("table").attr("height", 200).attr("width", 400),
+      thd = tbl.append("thead")
+      tbd = tbl.append("tbody"),
+      rows = tbd.selectAll("tr").data(colors).enter().append("tr");
+
+  tbl.style("display", "inline-block");
+  thd.style("display", "block");
+  tbd.style("overflow-y", "scroll").style("display", "block");
+
+  thd.append("td").text("");
+  thd.append("td").text("l*");
+  thd.append("td").text("a*");
+  thd.append("td").text("b*");
+  thd.append("td").text("rgb");
+
+  function precisionRound(number, precision) {
+    var factor = Math.pow(10, precision);
+    return Math.round(number * factor) / factor;
+  }
+  rows.each(function(d) {
+    var r = d3.select(this);
+    r.append("td").attr("width", 10).style("background-color", d);
+    r.append("td").text(precisionRound(d.l, 2));
+    r.append("td").text(precisionRound(d.a, 2));
+    r.append("td").text(precisionRound(d.b, 2));
+    r.append("td").text(d + "");
+  });
+}
---
+++ js/uploadImage.js
@@ -0,0 +1,97 @@
+var makeImageColorPicker = function(container) {
+  var canvas = container.select("canvas").node(),
+      context = canvas.getContext("2d"),
+      colorList = container.select(".uniqueColors"),
+      dropArea = container.select(".dragArea"),
+      dropImg = container.select(".dropImg"),
+      uploadBtn = container.select(".uploadBtn");
+
+  dropArea.on("dragover", dragOverArea);
+  dropArea.on("drop", selectFile);
+  dropImg.on("load", buildCanvas);
+  uploadBtn.on("click", function() {
+    var localFile = container.select(".uploadForm input[type=\"file\"]");
+    if(localFile.property("files").length > 0) {
+      var reader = new FileReader();
+      reader.onload = function(e) { dropImg.node().src=e.target.result; };
+      reader.readAsDataURL(localFile.property("files")[0]);
+    }
+  });
+
+  function buildCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(dropImg.node(), 0, 0, canvas.width, canvas.height);
+
+    var image = context.getImageData(0, 0, canvas.width, canvas.height),
+        pxs = Array(canvas.width*canvas.height);
+
+    // quickly iterate over all pixels
+    var i = -1;
+    for(var x = 0; x < canvas.width*canvas.height; ++x) {
+      pxs[x] = d3.hcl(d3.rgb(image.data[++i],
+                             image.data[++i],
+                             image.data[++i]).toString());
+      ++i;
+    }
+
+    var uniqueColors = pxs.filter(function(d, i, self) {
+              return self.indexOf(d) === i;
+            })
+            .reduce(function(acc, d) {
+              if(acc.length === 0) return [d];
+
+              var i = 0, isND;
+              var dStr = d + "";
+              for(i = 0; i<acc.length; i++) {
+                if(acc[i]+"" === dStr) return acc;
+              }
+              acc.push(d);
+              return acc;
+            }, []).sort(function(a,b) {
+              var hDiff = a.h - b.h;
+              if(Math.abs(hDiff) > 15) return hDiff;
+              var lDiff = a.l - b.l;
+              if(Math.abs(lDiff) > 5) return lDiff;
+              else return a.c - b.c;
+            });
+
+    colorList.selectAll("li").remove();
+    colorList.selectAll("li")
+        .data(uniqueColors)
+        .enter()
+        .append("li")
+            .style("width", "25px").style("height", "25px")
+            .style("border-radius", "50%")
+            .style("display", "inline-block")
+            .style("background-color", d => d);
+  }
+
+  function dragOverArea() {
+    d3.event.stopPropagation();
+    d3.event.preventDefault();
+    d3.event.dataTransfer.dropEffect = "copy";
+  }
+
+  function selectFile() {
+    d3.event.stopPropagation();
+    d3.event.preventDefault();
+
+    var files = d3.event.dataTransfer.files;
+    if(files.length < 1) return;
+
+    var file = files[0];
+    if (!file.type.match("image.*")) {
+        return;
+    }
+
+    var reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = function(e) {
+      dropImg.node().src=e.target.result;
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+  }
+};
